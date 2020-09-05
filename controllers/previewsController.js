4cb750eab7cea9f4881857af96fc5b2ba92b6ffa
mvciekzvbek <maciej.jan.zabek@gmail.com>
2020-09-05 22:26:56 +0200

    s3 upload

--- controllers/previewsController.js
+++ controllers/previewsController.js
@@ -1,8 +1,13 @@
 import DbService from '../services/DbService';
 
 export default {
-  async findOne(req, res, next) {
+  async findOne(req, res) {
     const { uuid } = req.params;
+
+    if (!uuid) {
+      return res.status(400);
+    }
+
     const record = await DbService.getPreviewByUuid(uuid);
     return record ? res.status(200).send(record)
       : res.sendStatus(404);
--- controllers/spreadsheetsController.js
+++ controllers/spreadsheetsController.js
@@ -1,14 +1,32 @@
 import SpreadSheetService from '../services/SpreadSheetService';
 import DbService from '../services/DbService';
+import S3Service from '../services/S3Service';
 import SpreadSheetPreview from '../models/SpreadSheetPreview';
 
 export default {
-  async upload(req, res, next) {
+  async upload(req, res) {
+    if (!req.file) {
+      return res.status(400);
+    }
+
     const spreadSheetService = new SpreadSheetService(req.file);
     const preview = spreadSheetService.generatePreview();
     const spreadSheetPreview = new SpreadSheetPreview(preview);
 
-    const record = await DbService.savePreview(spreadSheetPreview);
+    const response = await S3Service.upload(spreadSheetPreview.uuid,
+      req.file.buffer,
+      req.file.mimetype);
+
+    if (!response || !response.Location) {
+      return res.status(400);
+    }
+
+    const record = await DbService.savePreview(spreadSheetPreview, response.Location);
+
+    if (!record) {
+      return res.status(400);
+    }
+
     return res.status(201).send({
       uuid: record.uuid,
     });
