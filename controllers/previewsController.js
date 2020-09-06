e9b86db9b775de69363a03629feda5163be75a1b
mvciekzvbek <maciej.jan.zabek@gmail.com>
2020-09-06 21:29:31 +0200

    one more logger

--- controllers/previewsController.js
+++ controllers/previewsController.js
@@ -1,11 +1,18 @@
 import DbService from '../services/DbService';
 import dbProvider from '../providers/DbProvider';
+import logger from '../providers/LoggerProvider';
 
 export default {
   async findOne(req, res) {
     const { uuid } = req.params;
     const dbService = DbService(dbProvider);
     const record = await dbService.getPreviewByUuid(uuid);
-    return record ? res.status(200).send(record) : res.sendStatus(404);
+    if (!record) {
+      logger.error('Preview does not exist in database');
+      return res.status(404).send({
+        message: 'Preview does not exist in database'
+      });
+    }
+    return res.status(200).send(record);
   },
 };
